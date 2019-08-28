b2647e602bda4415d000396ecf442dc2935e0913
Henry <henry.lin@qbssystem.com>
2019-08-28 11:48:38 +0800

    Initial Commit

---
+++ Git-Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git-Practice
+//
+//  Created by Henry on 2019/8/28.
+//  Copyright © 2019 Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
